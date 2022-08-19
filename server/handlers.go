7d2d05935b9dc2ed89527b70a225489221f7f8ce
Shantanu <sgkandale.gmail.com>
2022-08-19 07:06:01 +0530

    path grouping

---
+++ server/handlers.go
@@ -0,0 +1,15 @@
+package server
+
+import "github.com/gin-gonic/gin"
+
+func (s Server) login(c *gin.Context) {}
+
+func (s Server) logout(c *gin.Context) {}
+
+func (s Server) getHost(c *gin.Context) {}
+
+func (s Server) addHost(c *gin.Context) {}
+
+func (s Server) updateHost(c *gin.Context) {}
+
+func (s Server) deleteHost(c *gin.Context) {}
--- server/routes.go
+++ server/routes.go
@@ -1,11 +1,22 @@
 package server
 
-import "github.com/gin-gonic/gin"
+func getPath(pathPrefix, path string) string {
+	if pathPrefix == "" {
+		return path
+	}
+	return pathPrefix + path
+}
 
-func (s Server) getHost(c *gin.Context) {}
+func (s Server) addRoutes(pathPrefix string) {
 
-func (s Server) addHost(c *gin.Context) {}
+	userRoutes := s.Handler.Group(getPath(pathPrefix, "/user"))
+	hostRoutes := s.Handler.Group(getPath(pathPrefix, "/host"))
 
-func (s Server) updateHost(c *gin.Context) {}
+	userRoutes.POST("/login", s.login)
+	userRoutes.POST("/logout", s.logout)
 
-func (s Server) deleteHost(c *gin.Context) {}
+	hostRoutes.GET("/", s.getHost)
+	hostRoutes.POST("/", s.addHost)
+	hostRoutes.PUT("/", s.updateHost)
+	hostRoutes.DELETE("/", s.deleteHost)
+}
